0239da065fa58881418814bbf9006bb6c4e92315
devangelaguilar <devangelaguilar@gmail.com>
2025-03-20 11:29:15 -0600

    Frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'productos',
+        component: ProductosComponent
+    }
+
+
+];
